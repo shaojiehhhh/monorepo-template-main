fea70533958b77e89f6305c6171b17347e8fb3b9
shajiehhh <huang.shaoj@northeastern.edu>
2023-09-12 12:42:38 -0700

    refactor code

--- Lab1/src/rectangle.py
+++ Lab1/src/rectangle.py
@@ -5,21 +5,55 @@
  Note that there is no constructor or destructor,
  so a default one will be created for us.
 """
-class Rectangle:
-    def set_values(self, x, y):
-        self.width = x
-        self.height = y
+# class Rectangle:
+#     def set_values(self, x, y):
+#         self.width = x
+#         self.height = y
+
+#     def area(self):
+#         return self.width * self.height
+
+
+# if __name__ == "__main__":
+#     # Create a rectangle object
+#     rect = Rectangle()
+
+#     # Call a member function
+#     rect.set_values(3, 4)
+
+#     # Print out the area function
+#     print("area:", rect.area())
+
+'''''
+The Refactored code is as follows
+'''
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+
+    @abstractmethod
+    def set_values(self, width, height):
+        pass
+
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def set_values(self, width, height):
+        self._width = width
+        self._height = height
 
     def area(self):
-        return self.width * self.height
-
+        return self._width * self._height
 
 if __name__ == "__main__":
     # Create a rectangle object
-    rect = Rectangle()
+    rect = Rectangle(3, 4)
 
-    # Call a member function
-    rect.set_values(3, 4)
-
-    # Print out the area function
-    print("area:", rect.area())
+    # Call set_values and area methods
+    rect.set_values(5, 6)
+    print("Area:", rect.area())
